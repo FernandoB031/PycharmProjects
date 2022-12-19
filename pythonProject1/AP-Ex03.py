104612ff92047cae259bb3325fe35c577dc15b06
Fernando <117295352+FernandoB031@users.noreply.github.com>
2022-12-18 21:59:34 -0300

    Delete AP-Ex03.py

--- pythonProject1/AP-Ex03.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-files_store = 'D:\Estudos\Faculdade Uninter - Redes de computadores\C-Fase 2 2022\Linguagem de programação\Aula 07 - Atividade Prática\Stores.csv'
-df = pd.read_csv(files_store, sep=',')
-df.head()
-
-df.columns.values
